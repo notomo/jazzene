4d96e8129e19a572d88f4c88c264a2403f2a4c03
notomo <notomo.motono@gmail.com>
2026-01-02 17:00:47 +0900

    chore: deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { moonbit } from "vite-plugin-moonbit";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
+  base: "/jazzene/",
   plugins: [
     moonbit({
       watch: true,
